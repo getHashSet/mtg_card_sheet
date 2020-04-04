085f5ecd4b55f32f652a264d93dc2ba32fef810b
Matthew <matthew_carpenter@icloud.com>
2020-04-03 18:20:43 -0700

    updated

--- client/src/components/SearchCard/index.jsx
+++ client/src/components/SearchCard/index.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios';
 
-export default function SeachCard() {
+export default class SearchCard extends Component {
+    constructor(props) {
+        super(props)
     
-    return (
-        <div className="search_card_root">
-            <input type="text"/>
-            <div className="btn">Search Card</div>
-            <p>output</p>
+        this.state = {
+             cardName: "hushbringer",
+             cardData: "/hush.jpg"
+        }
+    }
+
+    seachThisCardName = () => {
+        console.log("seaching.");
+        axios({
+            method: 'get',
+            url: `https://api.scryfall.com/cards/named?fuzzy=${this.state.cardName}`,
+        })
+        .then(scryfallData => {
+            console.log(scryfallData.data)
+            this.setState({
+                cardData: scryfallData.data.image_uris.large
+            });
+
+        })
+        .catch(err => {
+            console.log('error');
+        });
+    };
+
+    inputChange = (event) => {
+        this.setState({
+            cardName: event.target.value
+        });
+    };
+    
+    render() {
+        return (
+            <div className="search_card_root">
+            <input type="text" onChange={this.inputChange} placeholder="Card Name"/>
+            <div className="btn" onClick={this.seachThisCardName}>Search Card</div>
+            <img src={this.state.cardData} alt="a magic card" />
         </div>
-    )
+        )
+    }
 }
