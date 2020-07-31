2bf6054a9598b11dcd61bdc12a1254783337de8f
Matthew <matthew_carpenter@icloud.com>
2020-07-30 19:00:43 -0700

    updated

--- controllers/deck.controller.js
+++ controllers/deck.controller.js
@@ -4,9 +4,9 @@ const axios = require("axios");
 router.route('/:id').get((req, res) => {
     let imageName = req.params.id;
     console.log('start')
-    console.log( path.join(__dirname, `../decks/${imageName}.jpg`));
+    console.log(`https://mtgchadbucket.s3-us-west-1.amazonaws.com/${imageName}.jpg`);
 
-    res.sendFile(path.join(__dirname, `../decks/${imageName}.jpg`));
+    res.redirect(`https://mtgchadbucket.s3-us-west-1.amazonaws.com/${imageName}.jpg`);
   
 });
 
