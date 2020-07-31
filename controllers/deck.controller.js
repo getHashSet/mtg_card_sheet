7293edff33ffc69b4bb1b3a519ab15b62d37ed20
Matthew <matthew_carpenter@icloud.com>
2020-07-30 19:00:07 -0700

    updaed.

--- controllers/deck.controller.js
+++ controllers/deck.controller.js
@@ -4,9 +4,9 @@ const axios = require("axios");
 router.route('/:id').get((req, res) => {
     let imageName = req.params.id;
     console.log('start')
-    console.log(`https://manaleaks.com/${imageName}.jpg`);
+    console.log( path.join(__dirname, `../decks/${imageName}.jpg`));
 
-    res.redirect(`https://manaleaks.com/${imageName}.jpg`);
+    res.sendFile(path.join(__dirname, `../decks/${imageName}.jpg`));
   
 });
 
