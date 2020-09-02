4c10f54733cc44c619615d9292e40d5a885452ca
Matthew <matthew_carpenter@icloud.com>
2020-09-01 17:19:07 -0700

    update

--- server.js
+++ server.js
@@ -106,11 +106,11 @@ app.get("*", (req, res) => {
 // ========================= //
 
 
-app.use(function (err, req, res, next) {
-	// console.log('====== ERROR =======')
-	// console.error(err.stack)
-	res.status(500)
-})
+// app.use(function (err, req, res, next) {
+// 	// console.log('====== ERROR =======')
+// 	// console.error(err.stack)
+// 	res.status(500)
+// })
 
 
 // ========================== //
